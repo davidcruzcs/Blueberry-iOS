e4551195424ea03768be576bbbbaba21b2926757
David Cruz <juandavidcruzs@gmail.com>
2018-09-25 22:42:52 -0500

    First Commit 🍇

---
+++ Blueberry/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Blueberry
+//
+//  Created by Juan David Cruz Serrano on 9/25/18.
+//  Copyright © 2018 Juan David Cruz Serrano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
